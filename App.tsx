b35250920b9cfb0341a37cdc2de8a7d05a2b5c63
Mário Fernandes <mariobmf02@gmail.com>
2020-06-07 14:10:16 -0300

    Added ESLint and Prettier, fixed styles

--- App.tsx
+++ App.tsx
@@ -1,14 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Initial Commit</Text>
-    </View>
-  );
-}
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -17,3 +9,10 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+const App: React.FC = () => (
+  <View style={styles.container}>
+    <Text>Initial Commit</Text>
+  </View>
+);
+export default App;
