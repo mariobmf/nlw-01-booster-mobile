01ec9619845042120ecb236aedc17af551379605
Mário Fernandes <mariobmf02@gmail.com>
2020-06-08 21:17:31 -0300

    Added all features of the home page

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,37 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+import React from 'react';
+import { View, ImageBackground, Image, Text } from 'react-native';
+import { Feather as Icon } from '@expo/vector-icons';
+import { RectButton } from 'react-native-gesture-handler';
+import styles from './styles';
+
+const logo = require('../../assets/logo.png');
+const imgBackground = require('../../assets/home-background.png');
+
+const Home: React.FC = () => {
+  return (
+    <ImageBackground
+      source={imgBackground}
+      style={styles.container}
+      imageStyle={{ width: 274, height: 368 }}
+    >
+      <View style={styles.main}>
+        <Image source={logo} />
+        <Text style={styles.title}>Seu marketplace de coleta de resíduos</Text>
+        <Text style={styles.description}>
+          Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente.
+        </Text>
+      </View>
+      <View style={styles.footer}>
+        <RectButton style={styles.button} onPress={() => {}}>
+          <View style={styles.buttonIcon}>
+            <Icon name="arrow-right" color="#FFF" size={24} />
+          </View>
+          <Text style={styles.buttonText}>Entrar</Text>
+        </RectButton>
+      </View>
+    </ImageBackground>
+  );
+};
+
+export default Home;
